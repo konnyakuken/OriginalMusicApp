5ceb1b5bb5491c25dadd06e196181acc7f6e7959
konnyakuken <t0516wakamiya0402@icloud.com>
2022-09-19 15:00:47 +0900

    firstcommit

---
+++ originalMusicApp/DB.swift
@@ -0,0 +1,27 @@
+//
+//  DB.swift
+//  originalMusicApp
+//
+//  Created by 若宮拓也 on 2022/09/19.
+//
+
+import Foundation
+import RealmSwift
+
+class Playlist: Object{
+    @objc dynamic var id: Int = 0
+    @objc dynamic var playlist_name: String = ""
+    
+    let musics = List<Music>()
+}
+
+class Music: Object{
+    @objc dynamic var id: Int = 0
+    @objc dynamic var spotify_id: String = ""
+    @objc dynamic var type: String = ""
+    @objc dynamic var artist: String = ""
+    @objc dynamic var album: String = ""
+    @objc dynamic var thumbnail: String = ""
+    @objc dynamic var name: String = ""
+    @objc dynamic var duration: Int = 0
+}
---
+++ originalMusicApp/ViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  originalMusicApp
+//
+//  Created by 若宮拓也 on 2022/09/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
